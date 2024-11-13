6733ee3cca3f92633edbaded64598238a9beba2b
Tomas <130842333+mattrotz@users.noreply.github.com.>
2024-11-13 18:18:48 -0500

    se completo home,se creo archivos auth y perfil, se completo los modelos

---
+++ abogados/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Templates/**/*.html',
+    './abogados/**/*.html',
+    // Agrega otras rutas de plantillas si es necesario
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- abogados/usuarios/models.py
+++ abogados/usuarios/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+# Models_Usuario
+class Usuario(models.Model): 
+    IdUsuario = models.CharField(max_length=20,unique=True)
+    nombre_usuario = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    contrasena = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nombre_usuario} ({self.IdUsuario}) ({self.email})"
+
+
+# Models_Abogados
+class Abogados(models.Model):
+    IdAbogado = models.CharField(max_length=20,unique=True)
+    especialidad = models.CharField(max_length=100)
+    nombre_abogado = models.CharField(max_length=100)
+    email = models.CharField(max_length=50,unique=True)
+    caso_actual = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.IdAbogado} ({self.nombre_abogado}) ({self.especialidad}) ({self.email}) ({self.caso_actual})"
